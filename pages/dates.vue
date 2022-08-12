dbd7ea04b1f7ea34090dc6edfdc2a46c6f0fa80c
搋兔 <naruto6404@hotmail.com>
2022-08-12 09:26:34 +0800

    add: important dates

--- pages/dates.vue
+++ pages/dates.vue
@@ -9,7 +9,7 @@
         >
           <div class="font-bold text-gray-600">{{ date.name }}</div>
           <div class="py-1 text-lg font-bold">
-            <div :class="['text-[red]', date.isDescription && 'line-through']">
+            <div :class="['text-[red]', !date.isDescription && 'line-through']">
               {{ date.date }}
             </div>
             <div class="text-[red]">{{ date.extend }}</div>
@@ -38,47 +38,47 @@ export default {
   data() {
     return {
       dates: [
-        // {
-        //   name: 'Invited Session Proposals Due:',
-        //   date: '31 August, 2020',
-        //   extend: '',
-        //   isDescription: false,
-        // },
-        // {
-        //   name: 'Paper Abstract Submission Due:',
-        //   date: '31 July, 2020',
-        //   extend: '(Deadline Extended to 15 August, 2020)',
-        //   isDescription: true,
-        // },
-        // {
-        //   name: 'Notification of Acceptance:',
-        //   date: 'Notify immediately after being reviewed',
-        //   extend: '',
-        //   isDescription: false,
-        // },
-        // {
-        //   name: 'Final Paper Submission:',
-        //   date: '20 September, 2020',
-        //   extend: '(Deadline Extended to 4 October, 2020)',
-        //   isDescription: true,
-        // },
-        // {
-        //   name: 'Author Registration Due:',
-        //   date: '9 November, 2020',
-        //   extend: '(Early-bird Discounts: Before 15 September, 2020)',
-        //   isDescription: false,
-        // },
-        // {
-        //   name: 'IEEE Xplore Submission Due:',
-        //   date: 'Notify after receiving IEEE submission link',
-        //   extend: '',
-        //   isDescription: false,
-        // },
+        {
+          name: 'Invited Session Proposals Due:',
+          date: 'April 30, 2023',
+          extend: '',
+          isDescription: true,
+        },
+        {
+          name: 'Paper Abstract Submission Due:',
+          date: 'March 15, 2023',
+          extend: '',
+          isDescription: true,
+        },
+        {
+          name: 'Notification of Acceptance:',
+          date: 'April 10, 2023',
+          extend: '',
+          isDescription: true,
+        },
+        {
+          name: 'Final Paper Submission:',
+          date: 'April 30, 2023',
+          extend: '',
+          isDescription: true,
+        },
+        {
+          name: 'Author Registration Due:',
+          date: 'June 20, 2023',
+          extend: '(Early-bird Discounts: Before May 1, 2023)',
+          isDescription: true,
+        },
+        {
+          name: 'IEEE Xplore Submission Due:',
+          date: 'Notify after receiving IEEE submission link',
+          extend: '',
+          isDescription: true,
+        },
         {
           name: 'Conference Dates:',
           date: 'June 30 - July 3, 2023',
           extend: '',
-          isDescription: false,
+          isDescription: true,
         },
       ],
       emails: ['is3c2023@ncut.edu.tw'],
