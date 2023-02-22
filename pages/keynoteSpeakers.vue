0a394f46de26ed5e1361ed05bab277b5d2976677
搋兔 <naruto6404@hotmail.com>
2023-02-23 02:28:57 +0800

    remove: ad of inviting

--- pages/keynoteSpeakers.vue
+++ pages/keynoteSpeakers.vue
@@ -9,11 +9,11 @@
         />
 
         <!-- Inviting -->
-        <div
+        <!-- <div
           class="inset-0 mx-auto mt-10 flex w-5/6 max-w-xl items-center justify-center rounded-md bg-gray-100 py-10 text-xl font-bold text-gray-500"
         >
           Inviting
-        </div>
+        </div> -->
       </div>
     </ContentBlock>
   </Main>
