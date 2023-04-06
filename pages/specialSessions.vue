24938bf00eadc3c2be912cc4a7957562604e11b6
搋兔 <naruto6404@hotmail.com>
2023-04-06 11:06:00 +0800

    fix: IS02 and IS09 file

--- pages/specialSessions.vue
+++ pages/specialSessions.vue
@@ -143,7 +143,7 @@ export default {
             'yuhuei.cheng@gmail.com',
             'yudalinemail@gms.npu.edu.tw',
           ],
-          note: '',
+          note: 'IS09-special-session-proposals.pdf',
         },
         // {
         //   // IS-10
