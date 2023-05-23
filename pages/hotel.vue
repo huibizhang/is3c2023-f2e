cca6ee83e91664eb9542976ccf6d631715166e3e
搋兔 <naruto6404@hotmail.com>
2023-05-23 18:00:36 +0800

    update: hotel information

--- pages/hotel.vue
+++ pages/hotel.vue
@@ -2,7 +2,7 @@
   <Main pageName="Hotel Information">
     <ContentBlock title="Hotel Information">
       <div class="my-10 flex w-full items-center justify-center space-x-5">
-        <a class="block" href="files/飯店資訊-賀緹酒店.pdf" target="_blank">
+        <a class="block" href="/files/飯店資訊-賀緹酒店.pdf" target="_blank">
           <div
             class="rounded-lg border px-5 py-2 text-lg transition-all hover:border-green-600 hover:bg-green-50 active:scale-95"
           >
@@ -12,7 +12,7 @@
         <div class="text-sm">/</div>
         <a
           class="block"
-          href="files/hotel-information_HETI-HOTEL.pdf"
+          href="/files/hotel-information_HETI-HOTEL.pdf"
           target="_blank"
         >
           <div
